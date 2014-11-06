3628af884e9a830f96729ab877baecb357e01e6a
CodeNewa <ashish.shakya@gmail.com>
2014-11-05 18:13:01 -0600

    Added get list.

--- NancyStart/NancyStart/Modules/CarModule.cs
+++ NancyStart/NancyStart/Modules/CarModule.cs
@@ -11,6 +11,49 @@ namespace NancyStart.Modules
         public CarModule()
         {
             Get["/"] = _ => "Hello World";
+
+            Get["/car/{id}"] = parameters =>
+            {
+                int id = parameters.id;
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithModel(id);
+            };
+
+            Get["/{make}/{model}"] = _ =>
+            {
+
+                var list =  new List<Car>
+                {
+                    new Car{Id = 1, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 2, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 3, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 4, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 5, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 6, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 7, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 8, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 9, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 10, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 11, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 12, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 13, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 14, Make = _.make, Model = _.Model}
+                    ,new Car{Id = 15, Make = _.make, Model = _.Model}
+                };
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithModel(list);
+            };
         }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
     }
 }
