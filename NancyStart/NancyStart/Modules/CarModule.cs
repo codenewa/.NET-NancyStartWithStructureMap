955264b1f9e9190d5e749f9dc2ba6a9e7751cf91
CodeNewa <ashish.shakya@gmail.com>
2014-11-05 18:03:49 -0600

    Get/ added.

---
+++ NancyStart/NancyStart/Modules/CarModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyStart.Modules
+{
+    public class CarModule: NancyModule
+    {
+        public CarModule()
+        {
+            Get["/"] = _ => "Hello World";
+        }
+    }
+}
